09e372746d5493ec2199e82bf7e6a2be081e8e19
Brant Stuns <brantstuns@gmail.com>
2016-10-10 23:12:50 -0700

    Get all the way to checkout process. Need to ditch the pauses though and figure out how to click as soon as the button is visible to cut back on time

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ client
   .url('http://www.eastbay.com/product/model:249030/sku:AQ7651/' +
     'adidas-2015-crazylight-boost-primeknit-mens/andrew-wiggins/blue/white/')
   .click('#pdp_size_select')
+  .pause(500)
+  .click('[data-value*="9.5"]')
+  .click('#add_to_cart')
+  .pause(1000)
+  .click('[id*="checkout"]')
+  .click('[data-btnname*="checkout"')
+  .pause(500)
   .end();
