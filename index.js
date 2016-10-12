f02ad063b05f5e76dbe4152b78143c59e562367e
Brant Stuns <brantstuns@gmail.com>
2016-10-11 23:29:46 -0700

    Updated the wdio stuff to not use pause everywhere and be a little smarter about elements being visible and stuff, also I guess footlocker is the first announced site so gunna program for that first

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const options = {desiredCapabilities: {browserName: 'chrome'}}
 const client = webdriverio.remote(options);
 
 client
-  .init()
-  .url('http://www.eastbay.com/product/model:249030/sku:AQ7651/' +
-    'adidas-2015-crazylight-boost-primeknit-mens/andrew-wiggins/blue/white/')
-  .click('#pdp_size_select')
-  .pause(500)
-  .click('[data-value*="9.5"]')
-  .click('#add_to_cart')
-  .pause(1000)
-  .click('[id*="checkout"]')
-  .click('[data-btnname*="checkout"')
-  .pause(500)
+  .init(options)
+  .url('http://www.footlocker.com/product/model:260702/' +
+  'sku:31069100/nike-free-rn-flyknit-mens/white/black/')
+  .waitForExist('[data-modelsize*="9_5"]',750)
+  .click('[data-modelsize*="9_5"]')
+  .waitForEnabled('input[value*="Add To Cart"]', 750)
+  .click('input[value*="Add To Cart"]')
+  .waitForExist('[data-btnname*="FullCart"]', 10000)
+  .waitForVisible('[data-btnname*="FullCart"]', 10000)
+  .click('[data-btnname*="FullCart"]')
   .end();
+
+
+
