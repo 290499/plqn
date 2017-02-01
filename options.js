8c9670380a3530924398ca4dad2ebf621206650b
290499 <fan-cena@hotmail.fr>
2017-02-01 23:58:55 +0100

    Update options.js

--- options.js
+++ options.js
@@ -1,16 +1,16 @@
-module.exports = {
-  url: '',
-  size: '',
-  firstName: '',
-  lastName: '',
-  phoneNumber: '',
-  email: '',
+2module.exports = {
+  url: 'http://www.eastbay.com/product/model:271125/sku:80262010/nike-alpha-lunar-clipper-turf-2017-mens/black/white/',
+  size: '07.0',
+  firstName: 'theo',
+  lastName: 'plozer',
+  phoneNumber: '+330606060606',
+  email: 'ukebuxcwf@gmail.co;',
   shipping: {
-    addressLine1: '',
-    addressLine2: '',
-    city: '',
-    state: '',
-    zipCode: ''
+    addressLine1: 'blue str',
+    addressLine2: '13',
+    city: 'sqntq bqrqbqrq',
+    state: 'California',
+    zipCode: '92430'
   },
   billing: {
     addressLine1: '',
@@ -20,8 +20,8 @@ module.exports = {
     zipCode: ''
   },
   payment: {
-    number: '',
-    expiration: '', // Formatted with just four consecutive numbers. eg if your expiration is 10/17 just put 1017
-    csc: '' // this is that code on the front or back of your card
+    number: '4985489648474596',
+    expiration: '1218', // Formatted with just four consecutive numbers. eg if your expiration is 10/17 just put 1017
+    csc: '698' // this is that code on the front or back of your card
   }
 };
